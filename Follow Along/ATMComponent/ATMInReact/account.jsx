ceb1905bfff32fb2688e531b50ad58b1ec4af7f9
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-11-04 11:30:29 -0400

    removed bad practices

--- Follow Along/ATMComponent/ATMInReact/account.jsx
+++ Follow Along/ATMComponent/ATMInReact/account.jsx
@@ -11,15 +11,13 @@ const ATMDeposit = ({onChange}) => {
 const Account = () => {
   let transactionState = 0; // state of this transaction
   let totalState = 0; // Account total at Bank
-  let status = "Account Balance $zero";
+  let status = `Account Balance $ ${totalState}`;
   const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
     transactionState = Number(event.target.value);
   };
   const handleSubmit = () => {
     totalState += transactionState;
-    status = `Account Balance $ ${totalState}`;
-    document.getElementById("total").innerHTML = status;
     event.preventDefault();
   };
   return (
