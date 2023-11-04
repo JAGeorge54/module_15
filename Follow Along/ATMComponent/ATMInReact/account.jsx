18b47b68e774c86857680a4da390de849d33704c
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-11-04 11:12:26 -0400

    moved outside variables and function inside

--- Follow Along/ATMComponent/ATMInReact/account.jsx
+++ Follow Along/ATMComponent/ATMInReact/account.jsx
@@ -1,18 +1,17 @@
-let transactionState = 0; // state of this transaction
-let totalState = 0; // Account total at Bank
-let status = "Account Balance $zero";
-const handleChange = event => {
-  console.log(`handleChange ${event.target.value}`);
-  transactionState = Number(event.target.value);
-};
-const handleSubmit = event => {
-  totalState += transactionState;
-  status = `Account Balance $ ${totalState}`;
-  document.getElementById("total").innerHTML = status;
-  event.preventDefault();
-};
-
 const ATMDeposit = () => {
+  let transactionState = 0; // state of this transaction
+  let totalState = 0; // Account total at Bank
+  let status = "Account Balance $zero";
+  const handleChange = event => {
+    console.log(`handleChange ${event.target.value}`);
+    transactionState = Number(event.target.value);
+  };
+  const handleSubmit = event => {
+    totalState += transactionState;
+    status = `Account Balance $ ${totalState}`;
+    document.getElementById("total").innerHTML = status;
+    event.preventDefault();
+  };
   return (
     <label className="label huge">
       Deposit:
