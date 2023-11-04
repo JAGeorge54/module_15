70582b920da67fb05d32cb1807d3c0e03b9b2c3e
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-11-04 12:25:48 -0400

    added deposit state

--- Follow Along/ATMComponent/ATMInReact/account.jsx
+++ Follow Along/ATMComponent/ATMInReact/account.jsx
@@ -10,17 +10,18 @@ const ATMDeposit = ({onChange, isDeposit}) => {
 };
 
 const Account = () => {
-  let deposit = 0; // state of this transaction
+  const [deposit, setDeposit] = React.useState(0);
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
   let status = `Account Balance $ ${totalState}`;
   const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
-    deposit = Number(event.target.value);
+    setDeposit(Number(event.target.value));
   };
   const handleSubmit = (event) => {
     let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
     setTotalState(newTotal);
+    setDeposit(0);
     event.preventDefault();
   };
   return (
